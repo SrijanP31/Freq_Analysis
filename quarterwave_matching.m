2dfe305d0bb4e65b4fc1ae6930984c0cbd8d7ef8
Srijan Prasanna <143769604+SrijanP31@users.noreply.github.com>
2025-04-24 06:21:03 +0530

    Add files via upload

--- quarterwave_matching.m
+++ quarterwave_matching.m
@@ -4,18 +4,15 @@ Z0 = 50; %Characteristic impedance of line to be matched
 RL = [70,100,200]; %Load resistance
 Z1 = sqrt(Z0 .* RL); %Characteristic impedance of quater-wave transformer
 
-c = 3e8; %Speed of light
-
 freq = linspace(0,2e9,4000);
 
 freq_match = 1e9; %Impedance matching at 1GHz
-beta = pi*freq/(freq_match*2);
+theta = pi*freq/(freq_match*2);
 
 Zin = zeros([3,4000]);
 
-val = beta*l_match;
 for i=1:1:3
-    Zin(i,:) = Z1(i)*(RL(i)+1i*Z1(i)*tan(beta)) ./ (Z1(i)+1i*RL(i)*tan(beta));
+    Zin(i,:) = Z1(i)*(RL(i)+1i*Z1(i)*tan(theta)) ./ (Z1(i)+1i*RL(i)*tan(theta));
 end
 
 gamma= (Zin - Z0) ./ (Zin + Z0);
